82154b22a2e234721326cffb4553576f8e1d1dfb
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-03-20 15:51:02 +1100

    created grid element and action enum

---
+++ src/enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class GridElement(Enum):
+    """This enum represents the possible grid elements for the robot navigation problem."""
+    EMPTY_CELL = 0
+    WALL = 1
+    AGENT = 2
+    TARGET = 3
+
+
+class Action(Enum):
+    """This enum represents the available actions for the robot navigation problem."""
+    MOVE_UP = 0
+    MOVE_LEFT = 1
+    MOVE_DOWN = 2
+    MOVE_RIGHT = 3
