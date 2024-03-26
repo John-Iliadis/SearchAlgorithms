5d843624878faf690a7d6a62d81b43f528579180
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-03-26 18:23:56 +1100

    extended bidirectional A star

--- src/search_methods.py
+++ src/search_methods.py
@@ -36,7 +36,7 @@ class SearchMethod:
 class BreadthFirstSearch(SearchMethod):
     def __init__(self, problem: 'Problem'):
         super().__init__(problem)
-        self.method_name = "Breadth First Search"
+        self.method_name = "BFS"
 
     def solve(self):
         frontier = deque([Node(self.problem.initial)])
@@ -52,18 +52,16 @@ class BreadthFirstSearch(SearchMethod):
 
             expanded.add(node.state)
 
-            child_nodes = node.expand(self.problem)
-            self.nodes_created += len(child_nodes)
-
-            for child_node in child_nodes:
+            for child_node in node.expand(self.problem):
                 if child_node.state not in expanded:
                     frontier.append(child_node)
+                    self.nodes_created += 1
 
 
 class DepthFirstSearch(SearchMethod):
     def __init__(self, problem: 'Problem'):
         super().__init__(problem)
-        self.method_name = "Depth First Search"
+        self.method_name = "DFS"
 
     def solve(self):
         frontier = [Node(self.problem.initial)]
@@ -80,12 +78,12 @@ class DepthFirstSearch(SearchMethod):
             expanded.add(node.state)
 
             child_nodes = node.expand(self.problem)
-            child_nodes.reverse()
-            self.nodes_created += len(child_nodes)
+            child_nodes.sort(key=lambda l_node: l_node.action.value, reverse=True)
 
             for child_node in child_nodes:
                 if child_node.state not in expanded:
                     frontier.append(child_node)
+                    self.nodes_created += 1
 
 
 class DepthLimitedSearch(SearchMethod):
@@ -111,45 +109,18 @@ class DepthLimitedSearch(SearchMethod):
             expanded.add(node.state)
 
             child_nodes = node.expand(self.problem)
-            child_nodes.reverse()
-            self.nodes_created += len(child_nodes)
+            child_nodes.sort(key=lambda l_node: l_node.action.value, reverse=True)
 
             for child_node in child_nodes:
                 if child_node.state not in expanded:
                     frontier.append(child_node)
-
-
-class DepthLimitedSearchRecursive(SearchMethod):
-    def __init__(self, problem: 'Problem', depth_limit: int = 20):
-        super().__init__(problem)
-        self.method_name = "Depth Limited Search Recursive"
-        self.depth_limit = depth_limit
-
-    def solve(self):
-
-        def recursive_dls(node, problem, limit):
-            if problem.goal_test(node.state):
-                return node
-            elif limit == 0:
-                return None
-            else:
-                for child in node.expand(problem):
-                    result = recursive_dls(child, problem, limit - 1)
-                    if result is not None:
-                        return result
-                return None
-
-        self.goal_node = recursive_dls(Node(self.problem.initial), self.problem, self.depth_limit)
-
-
-class IterativeDeepeningSearchRecursive(SearchMethod):
-
+                    self.nodes_created += 1
 
 
 class IterativeDeepeningSearch(SearchMethod):
     def __init__(self, problem: 'Problem'):
         super().__init__(problem)
-        self.method_name = "Iterative Deepening Search"
+        self.method_name = "CUS1"
 
     def solve(self):
         for depth in range(sys.maxsize):
@@ -184,12 +155,10 @@ class BestFirstSearch(SearchMethod):
 
             expanded.add(node.state)
 
-            child_nodes = node.expand(self.problem)
-            self.nodes_created += len(child_nodes)
-
-            for child in child_nodes:
+            for child in node.expand(self.problem):
                 if child.state not in expanded:
                     frontier.append(child)
+                    self.nodes_created += 1
 
 
 class UniformCostSearch(BestFirstSearch):
@@ -207,7 +176,7 @@ class GreedyBestFirstSearch(BestFirstSearch):
         def f(node): return problem.heuristic(node.state)
         def g(node): return utils.get_action_value(node)
         super().__init__(problem, f, g)
-        self.method_name = "Greedy Best First Search"
+        self.method_name = "GBFS"
 
 
 class AStarSearch(BestFirstSearch):
@@ -215,7 +184,7 @@ class AStarSearch(BestFirstSearch):
         def f(node): return node.path_cost + problem.heuristic(node.state)
         def g(node): return utils.get_action_value(node)
         super().__init__(problem, f, g)
-        self.method_name = "A* Search"
+        self.method_name = "AS"
 
 
 class BidirectionalAStarSearch(SearchMethod):
@@ -224,79 +193,93 @@ class BidirectionalAStarSearch(SearchMethod):
             raise TypeError("This Bidirectional A* search class is designed specifically for the robot navigation problem")
 
         super().__init__()
-        self.method_name = "Bidirectional A* Search"
+        self.method_name = "CUS2"
 
         self.problem_forward = copy.deepcopy(problem)
-        self.problem_backward = self.make_backward_problem(problem)
+        self.problems_backward = self.make_backward_problems(problem)  # list of backward problems
 
         self.solution = None  # solution is a list of actions
         self.lowest_cost_so_far = numpy.inf  # the current lowest cost of one of the solutions found
 
-        self.f_forward = lambda node: node.path_cost + self.problem_forward.heuristic(node.state)
-        self.f_backward = lambda node: node.path_cost + self.problem_backward.heuristic(node.state)
         self.g = lambda node: utils.get_action_value(node)
 
-    def make_backward_problem(self, problem: 'RobotNavigationProblem'):
-        """In a backward problem, the initial and goal states are swapped. """
-
-        problem_backward = copy.deepcopy(problem)
-        closest_state = problem_backward.goal[0]
-        goal = problem.initial
-
-        # find the goal state closest to the initial state before swapping
-        for state in problem_backward.goal[1:]:
-            if numpy.hypot(goal.x - state.x, goal.y - state.y) < numpy.hypot(goal.x - closest_state.x, goal.y - closest_state.y):
-                closest_state = state
-
-        problem_backward.initial, problem_backward.goal = closest_state, [problem_backward.initial]
-        return problem_backward
-
     def solve(self):
-        frontier_forward = PriorityQueue(self.f_forward, self.g)
-        frontier_backward = PriorityQueue(self.f_backward, self.g)
-
+        frontier_forward = PriorityQueue(self.get_a_star_heuristic(self.problem_forward), self.g)
         frontier_forward.append(Node(self.problem_forward.initial))
-        frontier_backward.append(Node(self.problem_backward.initial))
+        self.nodes_created = 1
 
-        expanded_forward, expanded_backward = {}, {}
+        frontiers_backward = []
 
-        while frontier_forward and frontier_backward:
-            combined_f_values = self.get_f_value(frontier_forward) + self.get_f_value(frontier_backward)
+        # calculate backwards frontiers
+        for problem in self.problems_backward:
+            pq = PriorityQueue(self.get_a_star_heuristic(problem), self.g)
+            frontiers_backward.append(pq)
+            frontiers_backward[-1].append(Node(problem.initial))
+            self.nodes_created += 1
+
+        # only one of each expanded lists
+        expanded_forward = {}
+        expanded_backward = {}
+
+        while frontier_forward and any(frontier for frontier in frontiers_backward):
+            cheapest_backward_frontier = min(frontiers_backward, key=lambda frontier: self.get_f_value(frontier))
+
+            # the combined f values of the priority node in the forward frontier and the cheapest backward frontier
+            combined_f_values = self.get_f_value(frontier_forward) + self.get_f_value(cheapest_backward_frontier)
 
             if combined_f_values < self.lowest_cost_so_far:
-                if self.top_node_has_min_f_value(frontier_forward, frontier_backward):
-                    self.step('forward', self.problem_forward, frontier_forward, frontier_backward, expanded_forward, expanded_backward)
+                if self.top_node_has_min_f_value(frontier_forward, cheapest_backward_frontier):
+                    self.step('forward', frontier_forward, cheapest_backward_frontier, expanded_forward, expanded_backward)
                 else:
-                    self.step('backward', self.problem_backward, frontier_backward, frontier_forward, expanded_backward, expanded_forward)
+                    self.step('backward', cheapest_backward_frontier, frontier_forward, expanded_backward, expanded_forward)
             else:
                 break
 
-    def step(self, direction: str, problem_a, frontier_a, frontier_b, expanded_a, expanded_b):
-        """Expands a node in one direction and checks for a solution."""
+    def step(self, direction: str, frontier_a, frontier_b, expanded_a, expanded_b):
+        """Expands a node and checks for a solution."""
         node = frontier_a.pop()
         state = node.state
 
         expanded_a[state] = node
 
         # solution found when expanded node is in the other direction's expanded list
-        if node.state in expanded_b:
+        if state in expanded_b:
             # calculate new solution and lowest solution cost
             new_solution = self.make_solution(direction, node, expanded_b[state])
             self.lowest_cost_so_far = min(self.lowest_cost_so_far, frontier_a.f(node) + frontier_b.f(node))
+
             # if the new solution is cheaper, then it becomes the current solution
             if self.solution is None or len(new_solution) < len(self.solution):
                 self.solution = new_solution
 
         # expand frontier
-        child_nodes = node.expand(problem_a)
-        self.nodes_created += len(child_nodes)
-
-        for child in child_nodes:
+        # direction of problem doesn't matter when expanding node
+        for child in node.expand(self.problem_forward):
             state = child.state
             if state not in expanded_a:
                 frontier_a.append(child)
+                self.nodes_created += 1
 
-    def make_solution(self, direction: str, node_a, node_b):
+    def make_backward_problems(self, problem: 'RobotNavigationProblem') -> list:
+        """Creates backward problems where the initial and goal states are reversed. For each different goal state,
+        a new backward problem will be created."""
+
+        problems_backward = []
+
+        for goal_state in problem.goal:
+            new_backward_problem = copy.deepcopy(problem)
+            new_backward_problem.initial, new_backward_problem.goal = goal_state, [problem.initial]
+            problems_backward.append(new_backward_problem)
+
+        return problems_backward
+
+    def get_f_value(self, frontier):
+        """Returns the f value of the item with the highest priority in the priority queue."""
+        if frontier:
+            return frontier.f(frontier.peak())
+        return numpy.inf
+
+    def make_solution(self, direction: str, node_a: 'Node', node_b: 'Node') -> list:
         """The solution is a list of actions that combines node_a and node_b. The actions of the goal node are
         reversed, depending on the direction."""
         solution = []
@@ -305,11 +288,13 @@ class BidirectionalAStarSearch(SearchMethod):
             actions_a = node_a.solution()
             actions_b = [utils.reverse_action(action) for action in node_b.solution()]
             solution = actions_a + actions_b
+            self.goal_node = node_b.path()[0]
         elif direction == 'backward':
             actions_a = [utils.reverse_action(action) for action in node_a.solution()]
             actions_a.reverse()
             actions_b = node_b.solution()
             solution = actions_b + actions_a
+            self.goal_node = node_a.path()[0]
         else:
             raise RuntimeError("bidirectional_best_first_search::make_solution: 'direction' has invalid value")
 
@@ -325,12 +310,9 @@ class BidirectionalAStarSearch(SearchMethod):
 
         return f_value_a <= f_value_b
 
-    def get_f_value(self, frontier):
-        """Returns the f value of the item with the highest priority in the priority queue."""
-        return frontier.f(frontier.peak())
-
-    def is_solved(self):
-        return self.solution is not None
+    def get_a_star_heuristic(self, problem: 'RobotNavigationProblem'):
+        """Gets the ordering function for the priority queues of different problems."""
+        return lambda node: node.path_cost + problem.heuristic(node.state)
 
     def __repr__(self):
-        return f"{self.nodes_created}\n{self.solution}"
+        return f"{self.goal_node} {self.nodes_created}\n{self.solution}"
